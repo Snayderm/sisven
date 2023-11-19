c398f9523cf91d243ada8ff5af7e4af7fe8d1d83
Snayderm <sneyder1002m@gmail.com>
2023-11-19 13:54:31 -0500

    acomodacion tablas y categories corregido

--- database/migrations/2023_10_26_010052_create_invoices_table.php
+++ database/migrations/2023_10_26_010052_create_invoices_table.php
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('invoices');
+        Schema::dropIfExists('_invoices');
     }
 };
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\CategoryController;
+
+
 use Illuminate\Support\Facades\Route;
 
 /*
